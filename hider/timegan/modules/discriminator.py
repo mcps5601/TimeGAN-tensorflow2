d4e38234eec4dca00b13e483aac695cf8f5fdda4
mcps5601 <mcps5601@gmail.com>
2020-10-28 13:37:05 +0800

    modify the Joint Training part

--- hider/timegan/modules/discriminator.py
+++ hider/timegan/modules/discriminator.py
@@ -12,14 +12,16 @@ class Discriminator(tf.keras.Model):
         self.rnn_cells = tf.keras.layers.StackedRNNCells([
             self.rnn_cell for _ in range(self.num_layers)
         ])
-        self.rnn = tf.keras.layers.RNN(self.rnn_cells, return_sequences=True, 
-                                    return_state=True)
+        self.rnn = tf.keras.layers.RNN(self.rnn_cells,
+                                       return_sequences=True, 
+                                       stateful=False,
+                                       return_state=True)
         # self.rnn = tf.keras.models.Sequential([
         #     tf.keras.layers.RNN(self.rnn_cell) for _ in range(self.num_layers)
         # ])
         self.linear = tf.keras.layers.Dense(1, activation=None)
 
-    def call(self, H):
-        d_output, d_last_state = self.rnn(H)
-        Y_hat = self.linear(d_output)
+    def call(self, H, training=False):
+        d_output = self.rnn(H)
+        Y_hat = self.linear(d_output[0])
         return Y_hat
--- hider/timegan/timegan.py
+++ hider/timegan/timegan.py
@@ -8,7 +8,6 @@ from .modules.discriminator import Discriminator
 from .modules.model_utils import extract_time, random_generator, batch_generator, MinMaxScaler
 import logging, os , datetime
 logging.disable(logging.WARNING) 
-#os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 
 class TimeGAN(tf.keras.Model):
@@ -67,14 +66,23 @@ class TimeGAN(tf.keras.Model):
 
             if train_G:
                 # Generator loss
-                G_loss_U = tf.nn.sigmoid_cross_entropy_with_logits(tf.ones_like(Y_fake), Y_fake)
-                G_loss_U_e = tf.nn.sigmoid_cross_entropy_with_logits(tf.ones_like(Y_fake_e), Y_fake_e)
-                G_loss_S = tf.keras.losses.mean_squared_error(H[:, 1:, :], H_hat_supervise[:, :-1, :])
-                G_loss_V1 = tf.math.reduce_mean(tf.math.abs(tf.math.sqrt(tf.nn.moments(X_hat,[0])[1] + 1e-6)
-                                                - tf.math.sqrt(tf.nn.moments(X,[0])[1] + 1e-6)))
-                G_loss_V2 = tf.math.reduce_mean(tf.math.abs((tf.nn.moments(X_hat,[0])[0])
-                                                - (tf.nn.moments(X,[0])[0])))
+                G_loss_U = tf.math.reduce_mean(
+                    tf.nn.sigmoid_cross_entropy_with_logits(tf.ones_like(Y_fake), Y_fake)
+                )
+                G_loss_U_e = tf.math.reduce_mean(
+                    tf.nn.sigmoid_cross_entropy_with_logits(tf.ones_like(Y_fake_e), Y_fake_e)
+                )
+                G_loss_S = tf.math.reduce_mean(
+                    tf.keras.losses.mean_squared_error(H[:, 1:, :], H_hat_supervise[:, :-1, :])
+                )
+                # Difference in "variance" between X_hat and X
+                G_loss_V1 = tf.math.reduce_mean(tf.math.abs(tf.math.sqrt(tf.nn.moments(X_hat, [0])[1] + 1e-6)
+                                                - tf.math.sqrt(tf.nn.moments(X, [0])[1] + 1e-6)))
+                # Difference in "mean" between X_hat and X
+                G_loss_V2 = tf.math.reduce_mean(tf.math.abs((tf.nn.moments(X_hat, [0])[0])
+                                                - (tf.nn.moments(X, [0])[0])))
                 G_loss_V = G_loss_V1 + G_loss_V2
+                #G_loss_V = tf.math.add(G_loss_V1, G_loss_V2)
                 ## Sum of all G_losses
                 G_loss = G_loss_U + gamma * G_loss_U_e + 100 * tf.math.sqrt(G_loss_S) + 100 * G_loss_V
 
@@ -95,12 +103,12 @@ class TimeGAN(tf.keras.Model):
         elif train_D:
             D_var_list = self.discriminator.trainable_weights
             D_grads = tape.gradient(D_loss, D_var_list)
-            optimizer.apply_gradients(zip(grads, D_var_list))
+            optimizer.apply_gradients(zip(D_grads, D_var_list))
 
             return tf.math.reduce_mean(D_loss)
 
         elif not train_D:
-            print("Checking if D_loss > 0.15")
+            # Checking if D_loss > 0.15
             return tf.math.reduce_mean(D_loss)
 
     # E_solver
@@ -108,19 +116,22 @@ class TimeGAN(tf.keras.Model):
         with tf.GradientTape() as tape:
             H = self.embedder(X, training=True)
             X_tilde = self.recovery(H, training=True)
-            E_loss_T0 = tf.keras.losses.mean_squared_error(X, X_tilde)
+            E_loss_T0 = tf.math.reduce_mean(
+                tf.keras.losses.mean_squared_error(X, X_tilde)
+            )
             E_loss0 = 10 * tf.math.sqrt(E_loss_T0)
 
             H_hat_supervise = self.supervisor(H)
-            G_loss_S = tf.keras.losses.mean_squared_error(H[:, 1:, :], H_hat_supervise[:, :-1, :])
-         
+            G_loss_S = tf.math.reduce_mean(
+                tf.keras.losses.mean_squared_error(H[:, 1:, :], H_hat_supervise[:, :-1, :])
+            )
             E_loss = E_loss0 + 0.1 * G_loss_S
         
         var_list = self.embedder.trainable_weights + self.recovery.trainable_weights
         grads = tape.gradient(E_loss, var_list)
         optimizer.apply_gradients(zip(grads, var_list))
 
-        return tf.math.reduce_mean(E_loss)     
+        return E_loss
 
 
 def train_timegan(ori_data, mode, args):
@@ -140,6 +151,7 @@ def train_timegan(ori_data, mode, args):
         GS_solver = tf.keras.optimizers.Adam(epsilon=args.epsilon)
         G_solver = tf.keras.optimizers.Adam(epsilon=args.epsilon)
         E_solver = tf.keras.optimizers.Adam(epsilon=args.epsilon)
+        D_solver = tf.keras.optimizers.Adam(epsilon=args.epsilon)
         
         print('Set up Tensorboard')
         current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -148,11 +160,11 @@ def train_timegan(ori_data, mode, args):
 
         # 1. Embedding network training
         print('Start Embedding Network Training')
-        for itt in range(args.iterations):
+        for itt in range(1):
             X_mb, T_mb = batch_generator(ori_data, ori_time, args.batch_size)
             X_mb = tf.convert_to_tensor(X_mb, dtype=tf.float32)
             step_e_loss = model.recovery_forward(X_mb, E0_solver)
-            if itt % 100 == 0:
+            if itt % 10 == 0:
                 print('step: '+ str(itt) + '/' + str(args.iterations) +
                       ', e_loss: ' + str(np.round(np.sqrt(step_e_loss),4)))
                 # Write to Tensorboard
@@ -163,7 +175,7 @@ def train_timegan(ori_data, mode, args):
 
         # 2. Training only with supervised loss
         print('Start Training with Supervised Loss Only')
-        for itt in range(args.iterations):
+        for itt in range(1):
             X_mb, T_mb = batch_generator(ori_data, ori_time, args.batch_size)
             Z_mb = random_generator(args.batch_size, args.z_dim, T_mb, args.max_seq_len)
 
@@ -171,8 +183,12 @@ def train_timegan(ori_data, mode, args):
             Z_mb = tf.convert_to_tensor(Z_mb, dtype=tf.float32)
 
             step_g_loss_s = model.supervisor_forward(X_mb, Z_mb, GS_solver)
-            if itt % 1000 == 0:
-                print('step: '+ str(itt)  + '/' + str(args.iterations) +', s_loss: ' + str(np.round(np.sqrt(step_g_loss_s),4)))
+            if itt % 10 == 0:
+                print('step: '+ str(itt)  + '/' + str(args.iterations) +', s_loss: '
+                              + str(np.round(np.sqrt(step_g_loss_s),4)))
+                # Write to Tensorboard
+                with train_summary_writer.as_default():
+                    tf.summary.scalar('Supervised_loss', np.round(np.sqrt(step_g_loss_s),4), step=itt)
         
         print('Finish Training with Supervised Loss Only')
 
@@ -183,17 +199,47 @@ def train_timegan(ori_data, mode, args):
             for g_more in range(2):
                 X_mb, T_mb = batch_generator(ori_data, ori_time, args.batch_size)
                 Z_mb = random_generator(args.batch_size, args.z_dim, T_mb, args.max_seq_len)
-                step_g_loss_u, step_g_loss_s, step_g_loss_v = adversarial_forward(X_mb, Z_mb,
+                
+                X_mb = tf.convert_to_tensor(X_mb, dtype=tf.float32)
+                Z_mb = tf.convert_to_tensor(Z_mb, dtype=tf.float32)
+
+                step_g_loss_u, step_g_loss_s, step_g_loss_v = model.adversarial_forward(X_mb, Z_mb,
                                                                                   G_solver,
                                                                                   train_G=True,
                                                                                   train_D=False)
-                step_e_loss_t0 = embedding_forward_joint(X_mb, E_solver)
+                step_e_loss_t0 = model.embedding_forward_joint(X_mb, E_solver)
 
             # Discriminator training
             X_mb, T_mb = batch_generator(ori_data, ori_time, args.batch_size)
             Z_mb = random_generator(args.batch_size, args.z_dim, T_mb, args.max_seq_len)
-            check_d_loss = adversarial_forward(X_mb, Z_mb, G_solver, train_G=False, train_D=True)
 
+            X_mb = tf.convert_to_tensor(X_mb, dtype=tf.float32)
+            Z_mb = tf.convert_to_tensor(Z_mb, dtype=tf.float32)
 
-    
-    exit()
+            check_d_loss = model.adversarial_forward(X_mb, Z_mb, D_solver, train_G=False, train_D=False)
+            if (check_d_loss > 0.15): 
+                step_d_loss = model.adversarial_forward(X_mb, Z_mb, D_solver, train_G=False, train_D=True)
+            else:
+                step_d_loss = check_d_loss
+
+            if itt % 100 ==0:
+                print('step: '+ str(itt) + '/' + str(args.iterations) + 
+                      ', d_loss: ' + str(np.round(step_d_loss, 4)) + 
+                      ', g_loss_u: ' + str(np.round(step_g_loss_u, 4)) + 
+                      ', g_loss_s: ' + str(np.round(np.sqrt(step_g_loss_s), 4)) + 
+                      ', g_loss_v: ' + str(np.round(step_g_loss_v, 4)) + 
+                      ', e_loss_t0: ' + str(np.round(np.sqrt(step_e_loss_t0), 4)))
+                # Write to Tensorboard
+                with train_summary_writer.as_default():
+                    tf.summary.scalar('Joint/Discriminator',
+                                      np.round(step_d_loss, 4), step=itt)
+                    tf.summary.scalar('Joint/Generator',
+                                      np.round(step_g_loss_u, 4), step=itt)
+                    tf.summary.scalar('Joint/Supervisor',
+                                      np.round(step_g_loss_s, 4), step=itt)
+                    tf.summary.scalar('Joint/Moments',
+                                      np.round(step_g_loss_v, 4), step=itt)
+                    tf.summary.scalar('Joint/Embedding',
+                                      np.round(step_e_loss_t0, 4), step=itt)        
+        print('Finish Joint Training')
+        exit()
